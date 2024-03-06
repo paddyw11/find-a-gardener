7ceaf64e6f8e173737cb288dd5ac491461c2cddb
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-03-06 23:20:35 +0000

    added initial files

---
+++ findagardener/routes.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from findagardener import app, db
+#from findagardener.models import Category, Task
+
+@app.route("/")
+def home():
+    return render_template("base.html")
