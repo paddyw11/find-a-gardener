439dbda0b7372f8a38c3f1d7cbecbd691c4c3434
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-03-18 21:56:54 +0000

    built models for db schema

--- findagardener/models.py
+++ findagardener/models.py
@@ -1,15 +1,15 @@
 from findagardener import db
 
-class gardner_service_association(db.Model):
-    #schema for the association model
-    gardener_id = Column(Integer, ForeignKey('gardener.id'), primary_key=True)
-    service_id = Column(Integer, ForeignKey('service.id'), primary_key=True)
+class GardnerServiceAssociation(db.Model):
+    # schema for the association model
+    gardener_id = db.Column(db.Integer, db.ForeignKey('gardener.id'), primary_key=True)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), primary_key=True)
 
 class Service(db.Model):
     # schema for the Service model
     id = db.Column(db.Integer, primary_key=True)
     service_name = db.Column(db.String(25), unique=True, nullable=False)
-    service_desciption = db.Column(db.text, nullable=False)
+    service_description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Integer)
 
     def __repr__(self):
@@ -19,9 +19,14 @@ class Gardener(db.Model):
     # schema for the Gardener model
     id = db.Column(db.Integer, primary_key=True)
     gardener_name = db.Column(db.String(50), unique=True, nullable=False)
-    region_id = db.Column(db.ForeignKey("region.id", nullable=False))
-    region =db.relationship("Region", backref=db.backref("gardeners", lazy=True))
-    services_offered = db.Column(db.Integer, db.ForeignKey("service.id",), nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey("region.id"), nullable=False)
+    region = db.relationship("Region", backref=db.backref("gardeners", lazy=True))
+    services_offered = db.relationship("Service", secondary='gardner_service_association')
+
+    def __repr__(self):
+        return "#{0} - Gardener: {1} | Region: {2}".format(
+            self.id, self.gardener_name, self.region.region_name
+        )
 
 class Region(db.Model):
     # schema for the region model
--- findagardener/routes.py
+++ findagardener/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from findagardener import app, db
+from findagardener.models import GardnerServiceAssociation, Service, Gardener, Region
 #from findagardener.models import Category, Task
 
 @app.route("/")
