30d101da76f1af0bce7092a60692553c16963b15
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-03-27 18:06:35 +0000

    feat: created functioning form to add to service db table

--- findagardener/routes.py
+++ findagardener/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from findagardener import app, db
 from findagardener.models import GardnerServiceAssociation, Service, Gardener, Region
 #from findagardener.models import Category, Task
@@ -15,4 +15,12 @@ def services():
 
 @app.route("/add_service", methods=["GET", "POST"])
 def add_service():
+    if request.method == "POST":
+        service = Service(
+            service_name=request.form.get("service_name"),
+            service_description=request.form.get("service_description"),
+            price=request.form.get("price"))
+        db.session.add(service)
+        db.session.commit()
+        return redirect(url_for("services"))
     return render_template("add_service.html")
