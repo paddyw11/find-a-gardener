9b1602a16036c17bf1f8ec5938f76cd9832f50ac
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-03-29 18:21:32 +0000

    feat: added update and delete functions to service(s).

--- findagardener/routes.py
+++ findagardener/routes.py
@@ -10,7 +10,8 @@ def home():
 
 @app.route("/services")
 def services():
-    return render_template("services.html")
+    services = list(Service.query.order_by(Service.service_name).all())
+    return render_template("services.html", services=services)
 
 
 @app.route("/add_service", methods=["GET", "POST"])
@@ -24,3 +25,23 @@ def add_service():
         db.session.commit()
         return redirect(url_for("services"))
     return render_template("add_service.html")
+
+
+@app.route("/edit_service/<int:service_id>", methods=["GET", "POST"])
+def edit_service(service_id):
+    service = Service.query.get_or_404(service_id)
+    if request.method == "POST":
+        service.service_name = request.form.get("service_name")
+        service.service_description = request.form.get("service_description")
+        service.price = request.form.get("price")
+        db.session.commit()
+        return redirect(url_for("services"))
+    return render_template("edit_service.html", service=service)
+
+
+@app.route("/delete_service/<int:service_id>")
+def delete_service(service_id):
+    service = Service.query.get_or_404(service_id)
+    db.session.delete(service)
+    db.session.commit()
+    return redirect(url_for("services"))
